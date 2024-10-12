cc5fb791c380d1bf67dc18af95650839dfb13644
Arnaud Stadler <arnaudstadler@air-de-arnaud.home>
2024-10-12 17:15:11 +0200

    créa models et views

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.TextField(max_length=200)
+    project_link = models.URLField(max_length=200)
+    demo_link = models.URLField(max_length=200, blank=True, null=False)
+    image = models.ImageField(upload_to='project_images/', blank=True, null=False)
+
+    def __str__(self):
+        return self.title
+    
+class Skills(models.Model):
+    name = models.CharField(max_length=100)
+    level = models.CharField(max_length=100, blank=True, null=True)   # ex : "Débutant", "Intermédiaire", "Exepert"
+
+    def __str__(self):
+        return self.name
+    
+class DesiredProject(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    required_skills = models.ManyToManyField(Skills)
+
+    def __str__(self):
+        return self.title
+    
+class About(models.Model):
+    content = models.TextField()
--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from .models import Projects, Skills, DesiredProject, About
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
+def home(request):
+    return render(request, 'portfolio/templates/home.html')
+
+def project_list(request):
+    projects = Projects.objects.all()
+    return render(request, 'portfolio/templates/project_list.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = Projects.objects.get(id=pk)
+    return render(request, 'portfolio/templates/project_detail.html', {'project': project})
+
+def skill_list(request):
+    skills = Skills.objects.all()
+    return render(request, 'portfolio/templates/skills.html', {'skills': skills})
+
+def desired_project_list(request):
+    desired_projects = DesiredProject.objects.all()
+    return render(request, 'portfolio/templates/desired_projects.html', {'desired_projects': desired_projects})
+
+def about(request):
+    about_info = About.objects.first()
+    return render(request, 'portfolio/templates/about.html', {'about': about_info})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail(
+                f'Message de {name}',
+                message,
+                email,
+                ['arnaud.stadler@ikmail.com'],
+            )
+            return render(request, 'portfolio/templates/contact_success.html')
+    else:
+        form = ContactForm()
+    return render(request, 'portfolio/templates/contact.html', {'form': form})
